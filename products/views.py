22fe12623ecf0196d9ef70cd60e02ca8511c620f
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-12-12 08:24:07 +0000

    added products to view, and products url, and included the product url in the main urls.py file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
+
+def all_products(request):
+    """ A View to show all the products, including sorting and searching """
+
+    products = Products.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
