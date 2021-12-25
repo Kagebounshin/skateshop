a2f6d4968582b6a8d27669789b920fb89a6568f0
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-12-25 09:58:51 +0000

    Added sizes to product model, and size selector to product details page

--- bag/views.py
+++ bag/views.py
@@ -21,3 +21,20 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def add_to_bag(request, item_id):
+    """ Add a items to your shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
