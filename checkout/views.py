f48e4b2f20a981082a243bf34503647a16caa522
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-12-26 12:42:16 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KAvLUIyDn1HnQXRLofck57upbHbuGmjg5SiuOOrnHLhFFmnq02mpkLQgbdnIpR55etWUVExBtBQOzs2uP3tYuSL00Rj8n2Kie',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
