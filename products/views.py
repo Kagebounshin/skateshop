9894877c34b6beeaa912f830eac6f606511fbc30
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-12-28 14:28:48 +0000

    worked on layout, added crispy and margins to forms, styled the btn

--- products/views.py
+++ products/views.py
@@ -60,8 +60,9 @@ def product_detail(request, product_id):
     """ A View to show one specific product """
 
     product = get_object_or_404(Product, pk=product_id)
+
     context = {
-        'product': product
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
