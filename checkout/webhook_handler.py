0db167839758c8b4f38242b827214dd1f34e2def
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-01 12:22:57 +0000

    Worked on Stripe webhooks

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,5 +9,17 @@ class StripeWH_handler:
 
     def handle_event(self, event):
         """ Handle A Generic/Unknown/Unexpected Webhook Event"""
+        return HttpResponse(
+            content=f'Unhandled Webhook Recieved: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Handle The payment_intent.succeeded Webhook From Stripe"""
         return HttpResponse(content=f'Webhook Recieved: {event["type"]}',
                             status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Handle The Payment_intent.payment_failed Webhook From Stripe"""
+        return HttpResponse(
+            content=f'PAYEMENT FAILED Webhook Recieved: {event["type"]}',
+            status=200)
