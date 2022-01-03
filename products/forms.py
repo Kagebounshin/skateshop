2552f82fac8345034b85b54a28ad1da9bec3b30e
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-03 09:56:37 +0000

    Added add product temlplate, css, url and view

--- products/forms.py
+++ products/forms.py
@@ -10,9 +10,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.object.all()
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widfet.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-black rounded-0'
