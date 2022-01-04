449e85bdd818ac8a92c5c18923472c4604148385
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-04 14:06:59 +0000

    Removed

--- subscribe/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Subscribe
-
-
-class SubscribeNewsForm(forms.ModelForm):
-    """ Subscribe User To Newsletter """
-    class Meta:
-        model = Subscribe
-        fields = ('email',)
