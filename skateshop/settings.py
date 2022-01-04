870b701869e4ad51d3235aed3e94a26cad088d8b
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-04 15:04:01 +0000

    Removed secret key and set debug

--- skateshop/settings.py
+++ skateshop/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&z9(*msi9mia5hvfql$_k!2@oj_wib2k_5@8u^!+nhcxow1sw'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kagebounshin-skateshop.herokuapp.com', 'localhost']
 
