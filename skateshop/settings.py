8b84afe0af521fa1acfae435129637ca3d8c3f6b
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-05 09:07:57 +0000

    Added if statment to settings static, redownloaded the dependancies

--- skateshop/settings.py
+++ skateshop/settings.py
@@ -167,7 +167,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+if 'DEVELOPMENT' in os.environ:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+else : 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
