ed910fcdb7d211117d8c9bf4ba2b1060b517a1ce
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-01-29 10:50:31 +0000

    added dockstring

--- subscribe/tests.py
+++ subscribe/tests.py
@@ -5,6 +5,7 @@ from subscribe.forms import SubForm
 class TestSubForm(SimpleTestCase):
 
     def test_subscribe_is_required(self):
+        """ Testing required fields """
         form = SubForm({'email': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('email', form.errors.keys())
